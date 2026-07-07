4ac3cfe6a9bdfa1eee0356142bf009dd53449dd0
regisjonathan360 <regisjonathan360@users.noreply.github.com>
2026-07-06 23:17:57 -0400

    design: porter direction artistique cosmique sur page classements (fond profil-background, grain, nav, palette)

--- app-next/src/app/charts/layout.tsx
+++ app-next/src/app/charts/layout.tsx
@@ -1,8 +1,35 @@
+import Link from "next/link";
 import "./charts.css";
 
 export default function ChartsLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="hmi">
+      {/* Fond cosmique profil-background (desktop/mobile via CSS) */}
+      <div className="hmi__bg" aria-hidden="true" />
+      {/* Grain texture */}
+      <div className="hmi__grain" aria-hidden="true" />
+      {/* Scrim gradient pour la lisibilité */}
+      <div className="hmi__scrim" aria-hidden="true" />
+
+      {/* Navigation (portée du site statique) */}
+      <nav className="topbar" aria-label="Navigation principale">
+        <div className="topbar__inner">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img className="topbar__logo" src="/images/logo1.png" alt="" width={38} height={38} />
+          <Link className="topbar__brand" href="https://regisjonathan360.github.io/planete-hmi/">
+            Planète HMI
+          </Link>
+          <div className="topbar__nav">
+            <Link href="https://regisjonathan360.github.io/planete-hmi/">Accueil</Link>
+            <Link href="https://regisjonathan360.github.io/planete-hmi/artistes.html">Artistes</Link>
+            <Link href="/charts" className="is-active">Classements</Link>
+            <Link href="https://regisjonathan360.github.io/planete-hmi/actualites.html">Actualités</Link>
+            <Link href="https://regisjonathan360.github.io/planete-hmi/evenements.html">Événements</Link>
+          </div>
+        </div>
+      </nav>
+
+      {/* Contenu de la page */}
       <div className="hmi__wrap">{children}</div>
     </div>
   );
